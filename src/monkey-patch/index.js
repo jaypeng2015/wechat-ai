8e9a69e8159b3d854d5f87452c5016f99ccf6e1b
Jingwei Peng <jay.peng@inlight.com.au>
2017-05-23 13:52:30 +1000

    Reply messages via wechat web ui.

--- src/monkey-patch/index.js
+++ src/monkey-patch/index.js
@@ -1,20 +1,43 @@
 /* eslint-disable */
 (function(xhr) {
   const { ipcRenderer } = require('electron');
+  const triggerKeyDown = (element, keyCode) => {
+    const e =  new window.KeyboardEvent('keydown', {
+      bubbles: true,
+    });
+    delete e.keyCode;
+    Object.defineProperty(e, 'keyCode', {value: keyCode});
+    element.dispatchEvent(e);
+  };
   ipcRenderer.on('reply text', (event, message) => {
     const { user, response } = message;
-    console.log('reply to', user);
-    console.log('reply text', response.result.fulfillment.speech);
+    const element = document.querySelectorAll(`[data-username=\"${user}\"]`)[0];
+    element.click();
+    const editArea = document.getElementById('editArea');
+    editArea.innerHTML = response.result.fulfillment.speech.replace('\\n', '<br>');
+    angular.element(editArea).scope().editAreaCtn = editArea.innerHTML;
+    triggerKeyDown(editArea, 13); /* send the message */
   });
+  const findMe = () => {
+    const header = document.querySelectorAll('div[class=\"header\"]')[0];
+    if (!header) {
+      return null;
+    }
+
+    const tmp = header.innerHTML.substring(header.innerHTML.indexOf('username=') + 9);
+    return tmp.substring(0, tmp.indexOf('&'));
+  };
   const banana = (xhrInstance, events) => {
     if (xhrInstance.readyState == 4 && xhrInstance.status === 200 && events[0].target.responseText) {
       const response = JSON.parse(events[0].target.responseText);
+      const me = findMe();
       if (response.BaseResponse.Ret === 0 && response.AddMsgCount > 0) {
         const messages = response.AddMsgList.filter(message => (
+          message.ToUserName === me && (
           message.MsgType === 1 /* text message */
             || message.MsgType === 3 /* images */
             || message.MsgType === 34 /* voice message */
-            || message.MsgType === 49) /* sharing */
+            || message.MsgType === 49)) /* sharing */
         );
         if (messages.length > 0) {
           ipcRenderer.send('wechatMessage', messages);
